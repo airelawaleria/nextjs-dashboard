fc75378c864a787cf366e555e5f00253a983f1e7
Valeryia Andraichuk <airelawaleria@v-macbook-pro.speedport_w_724v_typ_a_05011603_06_003>
2024-10-29 13:44:50 +0100

    Implement customers page & fix linting errors

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,33 @@
+import { fetchCustomersPages } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+import Pagination from "@/app/ui/invoices/pagination";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Invoices",
 };
 
-export default () => {
-  return <p>Customers Page</p>;
-};
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await fetchCustomersPages(query);
+
+  return (
+    <div className="w-full">
+      <Suspense>
+        <CustomersTable query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
+}
